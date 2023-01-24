d19164f0c2a29202c296da49d8313f05e6663bac
Artur <artbalcer@gmail.com>
2023-01-24 01:02:35 +0100

    Initial Commit

---
+++ ToDo Watch App/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo Watch App
+//
+//  Created by Artur Balcer on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDo Watch AppUITests/ToDo_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDo_Watch_AppUITestsLaunchTests.swift
+//  ToDo Watch AppUITests
+//
+//  Created by Artur Balcer on 24/01/2023.
+//
+
+import XCTest
+
+final class ToDo_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
